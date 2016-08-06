e163cc4b6f978678c7eff616176b97fb3e81064a
Gabriel Redig <glredig@gmail.com>
2016-08-06 09:02:11 -0700

    fix annoying sublime spacing

--- validator.js
+++ validator.js
@@ -1,50 +1,50 @@
 function validator(str) {
-	var messages 	= str.replace(/\s+/g, ' ').split(' ');
+  var messages 	= str.replace(/\s+/g, ' ').split(' ');
 
-	for (var i = 0; i < messages.length; i++) {
-		validate(messages[i]);
-	}
+  for (var i = 0; i < messages.length; i++) {
+    validate(messages[i]);
+  }
 
-	function validate(message) {
-		// E : [MPKQ]EE | ZE | [abcdefghij]
-		var cursor = 0,
-			savedCursor = 0;
+  function validate(message) {
+    // E : [MPKQ]EE | ZE | [abcdefghij]
+    var cursor = 0,
+        savedCursor = 0;
 
 
-		function E() {
-			return (saveCursor(), E1()) || (backtrack(), saveCursor(), E2()) || (backtrack(), saveCursor(), E3());
-		}
+    function E() {
+      return (saveCursor(), E1()) || (backtrack(), saveCursor(), E2()) || (backtrack(), saveCursor(), E3());
+    }
 
-		// [MPKQ]EE
-		function E1() {
-			return (/[MKPQ]/.test(getNextToken()) && E() && E());
-		}
+    // [MPKQ]EE
+    function E1() {
+      return (/[MKPQ]/.test(getNextToken()) && E() && E());
+    }
 
-		// ZE
-		function E2() {
-			return (/Z/.test(getNextToken()) && E());
-		}
+    // ZE
+    function E2() {
+      return (/Z/.test(getNextToken()) && E());
+    }
 
-		// [a-j]
-		function E3() {
-			return (/[a-j]/.test(getNextToken()));
-		}
+    // [a-j]
+    function E3() {
+      return (/[a-j]/.test(getNextToken()));
+    }
 
-		function saveCursor() {
-			savedCursor = cursor;
-		}
+    function saveCursor() {
+      savedCursor = cursor;
+    }
 
-		function backtrack() {
-			cursor = savedCursor;
-		}
+    function backtrack() {
+      cursor = savedCursor;
+    }
 
-		function getNextToken() {
-			var nextToken = message[cursor];
-			cursor++;
-			return nextToken;
-		}
+    function getNextToken() {
+      var nextToken = message[cursor];
+      cursor++;
+      return nextToken;
+    }
 
-		console.log(message, ((E() && cursor == message.length) ? 'VALID' : 'INVALID'))
+    console.log(message, ((E() && cursor == message.length) ? 'VALID' : 'INVALID'))
 
-	}
+  }
 }
